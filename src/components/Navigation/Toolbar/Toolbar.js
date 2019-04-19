f49f2c18d78e11174870b6a67fd716fd908d27ff
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-04-19 18:54:17 +0000

    Add logo to header nav

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Toolbar.css';
+import Logo from '../../Logo/Logo';
 
 const Toolbar = props => (
   <header className='Toolbar'>
     <div>Menu</div>
-    <div>Logo</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );
