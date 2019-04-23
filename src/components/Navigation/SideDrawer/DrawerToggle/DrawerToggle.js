2e95d9f6fd44e8db3ca7a7dd72573e00c567c80a
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-04-23 19:47:36 +0000

    Adds a hamburger menu to the header

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './DrawerToggle.css';
 
-const drawerToggle = props => <div onClick={props.clicked}>Menu</div>;
+const drawerToggle = props => (
+  <div className='DrawerToggle' onClick={props.clicked}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
 
 export default drawerToggle;
