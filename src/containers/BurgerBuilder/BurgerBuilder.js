9f3c403d0d3f91ca218f2c7608d35e373a9e2895
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-04-25 18:31:46 +0000

    Navigates to the Checkout Page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,35 +93,36 @@ class BurgerBuilder extends Component {
   // This approach of calculating the price here is not recommended for
   // real projects, the should be done in the backend server for security reasons
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Ralf Pinheiro',
-        address: {
-          street: 'Test Street 1',
-          zipCode: '123',
-          country: 'Canada',
-          email: 'test@test.com'
-        },
-        deliveryMethod: 'Fastest'
-      }
-    };
-    axios
-      .post('/orders.json', order)
-      .then(response =>
-        this.setState({
-          loading: false,
-          purchasing: false
-        })
-      )
-      .catch(error =>
-        this.setState({
-          loading: false,
-          purchasing: false
-        })
-      );
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Ralf Pinheiro',
+    //     address: {
+    //       street: 'Test Street 1',
+    //       zipCode: '123',
+    //       country: 'Canada',
+    //       email: 'test@test.com'
+    //     },
+    //     deliveryMethod: 'Fastest'
+    //   }
+    // };
+    // axios
+    //   .post('/orders.json', order)
+    //   .then(response =>
+    //     this.setState({
+    //       loading: false,
+    //       purchasing: false
+    //     })
+    //   )
+    //   .catch(error =>
+    //     this.setState({
+    //       loading: false,
+    //       purchasing: false
+    //     })
+    //   );
+    this.props.history.push('/checkout');
   };
 
   render() {
