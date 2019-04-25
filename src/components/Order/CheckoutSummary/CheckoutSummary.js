9939eefb9584ba3c51bee00e6383039eba75c2d7
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-04-25 18:18:13 +0000

    Creates Checkout container

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import './CheckoutSummary.css';
+
+const CheckoutSummary = props => {
+  return (
+    <div clasName='CheckoutSummary'>
+      <h1>We hope it tastes well!</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button btnType='Danger' clicked>
+        Cancel
+      </Button>
+      <Button btnType='Success' clicked>
+        Continue
+      </Button>
+    </div>
+  );
+};
+
+export default CheckoutSummary;
--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Spinner from './Spinner.css';
+import './Spinner.css';
 
 const spinner = () => <div className='Loader'>Loading...</div>;
 
