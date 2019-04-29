91204f0de96cd1782c9dcac1aaf1f257aef99f26
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-04-29 19:15:38 +0000

    Adds user contact data component in the checkout process

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
@@ -38,6 +41,7 @@ class Checkout extends Component {
           CheckoutCancelled={this.checkoutCancelledHandler}
           CheckoutContinued={this.checkoutContinuedHandler}
         />
+        <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
       </div>
     );
   }
