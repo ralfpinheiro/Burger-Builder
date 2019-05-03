36eb0e68a351bdc7fbd88ad99acfef651a98144c
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-05-03 18:08:47 +0000

    Hnadle form submission and updates the database with the submitted data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -62,9 +62,14 @@ class ContactData extends Component {
   orderHandler = event => {
     event.preventDefault();
     this.setState({ loading: true });
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      orderData: formData
     };
     axios
       .post('/orders.json', order)
@@ -101,7 +106,7 @@ class ContactData extends Component {
     }
 
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input
             key={formElement.id}
@@ -111,9 +116,7 @@ class ContactData extends Component {
             changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-        <Button clicked={this.orderHandler} btnType='Success'>
-          Order
-        </Button>
+        <Button btnType='Success'>Order</Button>
       </form>
     );
     if (this.state.loading) {
