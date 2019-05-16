18d5fc0b4c3e148ca58c78326de6fdb6c5b0322b
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-05-16 11:44:47 -0700

    Adjusts Checkout and Contact Data using redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -49,16 +49,7 @@ class BurgerBuilder extends Component {
   // This approach of calculating the price here is not recommended for
   // real projects, the should be done in the backend server for security reasons
   purchaseContinueHandler = () => {
-    const queryParams = [];
-    for (let i in this.state.ingredients) {
-      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-    }
-    queryParams.push('price=' + this.state.totalPrice);
-    const queryString = queryParams.join('&');
-    this.props.history.push({
-      pathname: '/checkout',
-      search: '?' + queryString
-    });
+    this.props.history.push('/checkout');
   };
 
   render() {
