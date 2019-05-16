56f84396024039ce51dd4622281fd660ce50d7ce
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-05-16 13:34:28 -0700

    Re-organizes store folder structure

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from './store/reducer';
+import reducer from './store/reducers/reducer';
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
